8cfc1b5ceee7af5ba0ce71434a80598605a115d1
Shmuel Fomberg <shmuelfomberg@gmail.com>
2012-07-10 18:52:10 +0900

    blog config on save

--- plugins/MultiLangSite/lib/MT/MlsPlugin.pm
+++ plugins/MultiLangSite/lib/MT/MlsPlugin.pm
@@ -11,7 +11,7 @@ sub blog_config_template {
     my %groups = map { ( $_->groupid, $_ ) } @objs;
     my @out;
     foreach my $obj (values %groups) {
-        my (undef, undef, $name) = split '|', $obj->url;
+        my (undef, undef, $name) = split '\\|', $obj->url;
         my $rec = {
             group_id => $obj->groupid,
             group_name => $name,
@@ -28,16 +28,47 @@ sub blog_config_template {
 
 sub set_blog_group {
     my ( $cb, $plugin, $data ) = @_;
+    my $group_id = $data->{blog_groupid};
+    return 1 if $group_id eq '-1' or $group_id eq '0';
 
-    # $mt_support_save_config_filter = 1;
+    my ($id, $name) = $group_id =~ m/^(\d+)x(.*)$/;
+    return 1 unless defined $id;
 
-    # my $app = MT->instance;
+    my $app = MT->instance;
+    my $blog_id = $app->param('blog_id');
+    my $gclass = $app->model('mls_groups');
 
-    # my $facebook_api_key    = $data->{facebook_app_key};
-    # my $facebook_api_secret = $data->{facebook_app_secret};
+    if ($id > 0) {
+        # try and load the blog group object
+        my $blog_g = $gclass->load({ blog_id=>0, object_datasource=>'blog', object_id=>$blog_id });
+        if ($blog_g) {
+            if ($blog_g->groupid != $id) {
+                return $plugin->error('Can only set the blog group once');
+            } else {
+                # the group did not change, update the internal names
+                $blog_g->url(join('|', $data->{blog_internal_name}, $data->{blog_external_name}, $name));
+                $blog_g->save;
+                return 1;
+            }
+        }
+    }
 
-    # return __check_api_configuration( $app, $plugin, $facebook_api_key,
-    #     $facebook_api_secret );
+    my $blog_g = $gclass->new();
+    $blog_g->blog_id(0);
+    $blog_g->object_id($blog_id);
+    $blog_g->object_datasource('blog');
+    $blog_g->url(join('|', $data->{blog_internal_name}, $data->{blog_external_name}, $name));
+    $blog_g->groupid($id);
+    $blog_g->obj_rev(0);
+    $blog_g->is_primary(0);
+    $blog_g->save;
+
+    if ($id==0) {
+        # new group, need new number
+        $blog_g->groupid($blog_g->id);
+        $blog_g->save;
+    }
+    return 1;
 }
 
 1;
